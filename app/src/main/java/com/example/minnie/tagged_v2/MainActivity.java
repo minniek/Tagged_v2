115afb507199f7d307456b480af0f0c4b2be4700
Minnie Kim <minniek@bu.edu>
2015-02-20 12:50:54 -0500

    added HMAC signature (testing stage...)

--- app/src/main/java/com/example/minnie/tagged_v2/MainActivity.java
+++ app/src/main/java/com/example/minnie/tagged_v2/MainActivity.java
@@ -7,17 +7,20 @@
 
 package com.example.minnie.tagged_v2;
 
+import android.app.Activity;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.gson.Gson;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -28,14 +31,20 @@ import java.net.InetSocketAddress;
 import java.net.MalformedURLException;
 import java.net.Proxy;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Iterator;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+
+public class MainActivity extends Activity implements View.OnClickListener {
 
     // Declare widgets
     Button send, clear, compare;
@@ -43,14 +52,19 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     TextView responseTxt, reqHeadersOrigTxt, diffTxt;
 
     // PHP Server, Python Proxy variables
-    final String serverIP = "155.41.46.89"; final String serverPage = "server_v1.php";
-    final String proxyIP = "155.41.124.7"; final int proxyPort = 1717;
+    final String serverIP = "192.168.1.4"; final String serverPage = "server_v1.php";
+    final String proxyIP = "192.168.1.10"; final int proxyPort = 1717;
 
     String responseStr = "";
-    Map<String,String> origHeadersMap;
-    JSONObject origHeadersJSON, modHeadersJSON;
+    //Map<String, List<String>> requestProperties;
+    TreeMap<String,String> origHeadersMap, modHeadersMap;
+    //JSONObject origHeadersJSON;
+    JSONObject modHeadersJSON;
     StringBuilder diffHeaders;
     String TAG = "Tagged_v2";
+
+    // Crypto variables
+    private static final String HMAC_SHA1_ALGO = "HmacSHA1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,7 +92,10 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     // Assign tasks for buttons
     public void onClick(View v) {
         if (v == send) {
-            // First, check for network connection
+
+            diffTxt.setText(""); // Clear out JSONException message from previous send if needed...
+
+            // Check for network connection
             String stringURL = urlTxt.getText().toString();
             ConnectivityManager connMgr = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
             NetworkInfo nwInfo = connMgr.getActiveNetworkInfo();
@@ -92,6 +109,10 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         }
 
         if (v == clear) {
+            responseStr = "";
+            //origHeadersJSON = new JSONObject();
+            modHeadersJSON = new JSONObject();
+
             responseTxt.setText("");
             reqHeadersOrigTxt.setText("");
             diffTxt.setText("");
@@ -100,36 +121,28 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         if (v == compare) {
             diffHeaders = new StringBuilder();
             try {
-                modHeadersJSON = new JSONObject(responseStr);
+                // Convert JSONObject to Map
+                modHeadersJSON = new JSONObject(responseStr); // responseStr stores PHP server's output (i.e. the modified headers)
                 Log.d(TAG, "modHeadersJSON.toString(): " + modHeadersJSON.toString()); // DEBUGGING
+                modHeadersMap = new TreeMap<>();
+                modHeadersMap = new Gson().fromJson(modHeadersJSON.toString(), modHeadersMap.getClass());
+                Log.d(TAG, "modHeadersMap.toString(): " + modHeadersMap.toString());
+                responseTxt.setText(modHeadersMap.toString()); // Display modified headers
 
-                // TODO Use Gson or Jackson to convert JSONObject to Map...
-                Map<String,String> modHeadersMap = new HashMap<>();
-                Iterator modIter = modHeadersJSON.keys();
-                while(modIter.hasNext()) {
-                    String key = (String)modIter.next();
-                    try {
-                        String value = modHeadersJSON.getString(key);
-                        modHeadersMap.put(key, value);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-                Log.d(TAG, "modHeadersMap.toString(): " + modHeadersMap.toString()); // DEBUGGING
-
-                // Compare original with modified...display the diff
-                // Recall: a map will not contain duplicate keys
+                // TODO Test this logic more thoroughly...it doesn't handle the case if a key:value pair is only in the origHeadersMap -_-
+                // Compare original with modified and display the diff
                 for (Map.Entry<String, String> modHeader : modHeadersMap.entrySet()) {
-                    if ((!origHeadersMap.containsKey(modHeader.getKey())) || (!origHeadersMap.containsValue(modHeader.getValue()))) { // Case One: maps do not have same keys or same values
+                    // Case One: maps do not have same keys or same values
+                    if ((!origHeadersMap.containsKey(modHeader.getKey())) || (!origHeadersMap.containsValue(modHeader.getValue()))) {
                         diffHeaders.append(modHeader.getKey() + ":" + modHeader.getValue() + "\n");
-                    } else if (origHeadersMap.containsKey(modHeader.getKey())) { // Case Two: if both maps share the same key but have different values
+                        // Case Two: both maps share the same key but have different values
+                    } else if (origHeadersMap.containsKey(modHeader.getKey())) {
                         if (!origHeadersMap.get(modHeader.getKey()).contentEquals(modHeader.getValue())){
                             diffHeaders.append(modHeader.getKey() + ":" + modHeader.getValue() + "\n");
                         }
                     }
                 }
-
-                // If no difference is found...
+                // Case Three: no differences are found
                 if (origHeadersMap.equals(modHeadersMap)) { // Equivalent to "map1.entrySet().equals(map2.entrySet())
                     diffHeaders.append("No differences found.");
                 }
@@ -155,37 +168,42 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
         // onPostExecute displays the results of the AsyncTask
         protected void onPostExecute(String responseStr) {
-            responseTxt.setText(responseStr);
-            reqHeadersOrigTxt.setText(origHeadersJSON.toString());
+            reqHeadersOrigTxt.setText(origHeadersMap.toString()); // Display original request headers
+            responseTxt.setText(responseStr); // Display modified headers
         }
 
         private String connectToURL(String url) throws IOException {
             // Force HTTP requests to go through Proxy
             URL myURL = new URL(url);
             Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyIP, proxyPort));
-            HttpURLConnection conn = (HttpURLConnection)myURL.openConnection(proxy);
+            HttpURLConnection conn = (HttpURLConnection) myURL.openConnection(proxy);
 
             // Connect to URL and display HTML of Tagged PHP server
             try {
-                conn.setReadTimeout(10*1000); // milliseconds
-                conn.setConnectTimeout(10*1000); // milliseconds
+                conn.setReadTimeout(10 * 1000); // milliseconds
+                conn.setConnectTimeout(10 * 1000); // milliseconds
 
                 // Create JSON object to store original request headers
-                // NOTE: JSON objects do not allow duplicate keys and will override existing keys
-                origHeadersJSON = new JSONObject();
-                try {
-                    origHeadersJSON.put("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
-                    origHeadersJSON.put("Accept-Encoding", "gzip, deflate, sdch");
+                // NOTE: JSON objects do not allow duplicate keys and will override existing keys BUT keys are case-sensitive
+                //origHeadersJSON = new JSONObject();
+                //try {
+                //origHeadersJSON.put("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+                    /*origHeadersJSON.put("Accept-Encoding", "gzip, deflate, sdch");
                     origHeadersJSON.put("Accept-Language", "en-US,en;q=0.8");
-                    origHeadersJSON.put("Cache-Control", "max-age=123456789");
+                    origHeadersJSON.put("Cache-Control", "max-age=0");
+                    origHeadersJSON.put("Cache-Control", "max-age=123456789"); // TESTER - this will override the previous key-value assignment
                     origHeadersJSON.put("Connection", "close");
-                    origHeadersJSON.put("Connection", "open"); // TESTER - this will override the previous key-value assignment
+                    origHeadersJSON.put("key1", "value1");
+                    origHeadersJSON.put("KEY2", "value2");
+                    //origHeadersJSON.put("kEy2", "vAlue2"); // addRequestProperty() will treat this as key2=[value2, vAlue2]
                     origHeadersJSON.put("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36");
                 }  catch (JSONException e) {
                     e.printStackTrace();
                 }
-                Log.d(TAG, "origHeadersJSON.toString(): " + origHeadersJSON.toString()); // DEBUGGING
+                Log.d(TAG, "origHeadersJSON.toString(): " + origHeadersJSON.toString());
+                */
 
+                /*
                 // Convert JSON Object to HashMap
                 origHeadersMap = new HashMap<>();
                 Iterator origIter = origHeadersJSON.keys();
@@ -193,35 +211,96 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
                     String key = (String)origIter.next();
                     try {
                         String value = origHeadersJSON.getString(key);
-                        Log.d(TAG, "origHeadersJSON.get(key): " + origHeadersJSON.get(key).toString());
                         origHeadersMap.put(key, value);
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
                 }
-                Log.d(TAG, "origHeadersMap.toString(): " + origHeadersMap.toString()); // DEBUGGING
+                Log.d(TAG, "origHeadersMap.toString(): " + origHeadersMap.toString());
+                */
 
+                 /*
                 // Set request headers
-                // NOTE: addRequestProperty method will NOT override existing properties (i.e. duplicate header-value pairs are allowed)
+                // NOTE: addRequestProperty method will NOT override existing properties; if same header is found
                 for (Map.Entry<String, String> header : origHeadersMap.entrySet()) {
-                   conn.addRequestProperty(header.getKey(), header.getValue());
+                    conn.addRequestProperty(header.getKey(), header.getValue());
                 }
-                //conn.addRequestProperty("Accept-Language", "DUPE"); // TESTER - duplicate header will be added
-                //Log.d(TAG, "getRequestProperties.toString()" + conn.getRequestProperties().toString()); // DEBUGGING - returns Map<String, List<String>>
+                */
+                //Log.d(TAG, "getRequestProperties.toString()" + conn.getRequestProperties().toString().replace("[", "").replace("]", "")); // Returns Map<String, List<String>>
 
-                // Connect to URL to read and display HTML of Tagged Server (should show modified request headers made by Tagged Proxy)
+                // Set request headers using addRequestProperty()
+                // NOTE: addRequestProperty() method does NOT override existing values but it also does NOT duplicate keys; keys are case-insensitive
+                conn.addRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+                conn.addRequestProperty("Accept-Language", "en-US,en;q=0.8");
+                conn.addRequestProperty("Cache-Control", "max-age=0");
+                conn.addRequestProperty("Cache-Control", "max-age=123456789"); // TESTER - should store as Cache-Control=[max-age=0,max-age=123456789]
+                conn.addRequestProperty("Connection", "close");
+                conn.addRequestProperty("Key1", "value1");
+                conn.addRequestProperty("Key2", "value2");
+                //conn.addRequestProperty("kEy2", "vAlue2"); // TESTER - should store as Key2=[value2, vAlue2]
+                conn.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36");
+                Log.d(TAG, "getRequestProperties.toString() BEFORE HMAC Signature: " + conn.getRequestProperties().toString());
+
+                // Convert getRequestProperties() List parameter to String by creating new tree map to get sorted keys
+                origHeadersMap = new TreeMap<>();
+                for (Map.Entry<String, List<String>> entry : conn.getRequestProperties().entrySet()) {
+                    origHeadersMap.put(entry.getKey(), entry.getValue().toString().replace("[", "").replace("]", "")); // Remove brackets from list
+                    //origHeadersMap.put(entry.getKey(), entry.getValue().toString());
+                }
+                Log.d(TAG, "origHeadersMap.toString() BEFORE HMAC Signature: " + origHeadersMap.toString());
+
+/**********************************************************************************************************************************************************************/
+                // Create HMAC signature and send it as a header value ---> Test-Sig: <testHMACSig>
+                // Reference: http://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/AuthJavaSampleHMACSignature.html
+                String testHMACSig ="";
+                try {
+                    // Get a HMAC-SHA1 key from the raw key bytes
+                    String testKey = "this is not a real key";
+                    byte[] testKeyByte = testKey.getBytes();
+                    SecretKeySpec testSigningKey = new SecretKeySpec(testKeyByte, HMAC_SHA1_ALGO);
+
+                    // Get an HMAC-SHA1 Mac instance and initialize with the signing key
+                    Mac testMac = Mac.getInstance(HMAC_SHA1_ALGO);
+                    testMac.init(testSigningKey);
+
+                    // Compute the HMAC on input data bytes
+                    Log.d(TAG, "Data for HMAC: " + origHeadersMap.toString());
+                    byte[] testRawHmac = testMac.doFinal(origHeadersMap.toString().getBytes());
+
+                    // Base64-encode the HMAC
+                    testHMACSig = Base64.encodeToString(testRawHmac, 0); // 0 - Default
+                } catch (InvalidKeyException e) {
+                    e.printStackTrace();
+                    Log.e(TAG, "ERROR InvalidKeyException: " + e);
+                } catch (NoSuchAlgorithmException e){
+                    e.printStackTrace();
+                    Log.e(TAG, "ERROR NoSuchAlgorithmException: " + e);
+                }
+                Log.d(TAG, "testHMACSig: " + testHMACSig);
+                // Add HMAC signature as a request header value
+                conn.addRequestProperty("Test-Sig", testHMACSig);
+/**********************************************************************************************************************************************************************/
+                // Create new origHeadersMap to include the Test-Sig header
+                origHeadersMap = new TreeMap<>();
+                for (Map.Entry<String, List<String>> entry : conn.getRequestProperties().entrySet()) {
+                    origHeadersMap.put(entry.getKey(), entry.getValue().toString().replace("[", "").replace("]", "")); // Remove brackets from list
+                    //origHeadersMap.put(entry.getKey(), entry.getValue().toString());
+                }
+                Log.d(TAG, "origHeadersMap.toString() AFTER HMAC Signature: " + origHeadersMap.toString());
+
+                // Connect to URL and get PHP server content
                 conn.connect();
-                InputStream htmlContent = conn.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(htmlContent, "UTF-8"));
-                StringBuilder content = new StringBuilder();
+                InputStream PHPResponse = conn.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(PHPResponse, "UTF-8"));
+                StringBuilder PHPContent = new StringBuilder();
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    content.append(line);
+                    PHPContent.append(line);
                 }
 
-                // Close connection and return PHP server's HTML
+                // Close connection and return PHP server content
                 conn.disconnect();
-                return responseStr = content.toString();
+                return responseStr = PHPContent.toString();
             } catch (MalformedURLException e) {
                 e.printStackTrace();
                 return responseStr = "ERROR MalformedURLException caught: " + e;
@@ -230,5 +309,9 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
                 return responseStr = "ERROR IOException caught: " + e;
             }
         }
+    }
+
+    protected void onStop() {
+        super.onStop();
     }
 }
